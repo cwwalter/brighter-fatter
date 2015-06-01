d1e2aeae54d099ee17fe5f705058204e36f94b6e
Chris Walter <chris.walter@duke.edu>
2015-06-01 21:44:07 +0900

    These changes are all related to making the code PEP 008 compliant (with a few LSST modifications).

--- processSpots.py
+++ processSpots.py
@@ -4,16 +4,16 @@
 # C. Walter 01/2014
 
 import math
-import numpy                as np
-import pandas               as pd
+import numpy as np
+import pandas as pd
 import itertools
 
-import lsst.afw.math        as afwMath
-import lsst.afw.table       as afwTable
-import lsst.afw.image       as afwImg
+import lsst.afw.math as afwMath
+import lsst.afw.table as afwTable
+import lsst.afw.image as afwImg
 
 import lsst.meas.algorithms as measAlg
-import lsst.pex.logging     as pexLog
+import lsst.pex.logging as pexLog
 
 # Set the program verbosity level
 printLevel = 0
@@ -21,58 +21,58 @@ pexLog.Log.getDefaultLog().setThreshold(pexLog.Log.WARN)
 
 # Make a Panda hd5f data store and frame.
 h5store = pd.HDFStore('spotData.h5', mode='w')
-spots   = pd.DataFrame(columns = ('config', 'numElectrons',
-                        'ixx', 'errxx', 'iyy', 'erryy', 'stdx', 'stdy'))
+spots = pd.DataFrame(columns=('config', 'numElectrons',
+                     'ixx', 'errxx', 'iyy', 'erryy', 'stdx', 'stdy'))
 
 # File info
-outDir       = 'output/lsst_e_'
-suffix       = '_f2_R22_S11_E000.fits.gz'
+outDir = 'output/lsst_e_'
+suffix = '_f2_R22_S11_E000.fits.gz'
 
-electronLevel  = [1000, 2000, 3000, 4000, 5000, 
+electronLevel = [1000, 2000, 3000, 4000, 5000,
                  10000, 15000, 20000, 25000, 30000,
                  50000, 75000, 100000]
-    
+
 extraId = [0, 1, 2, 3, 4]
 
 # Set the statistics flags
-statFlags = (afwMath.NPOINT | afwMath.MEAN | afwMath.STDEV | afwMath.MAX | 
+statFlags = (afwMath.NPOINT | afwMath.MEAN | afwMath.STDEV | afwMath.MAX |
              afwMath.MIN | afwMath.ERRORS)
-print "The statistics flags are set to %s."%bin(statFlags)
+print "The statistics flags are set to %s." % bin(statFlags)
 print "Errors will be calculated.\n"
 
 # Configure the detection and measurement algorithms
-schema                = afwTable.SourceTable.makeMinimalSchema()
-detectSourcesConfig   = measAlg.SourceDetectionConfig(thresholdType='value')
-measureSourcesConfig  = measAlg.SourceMeasurementConfig()
+schema = afwTable.SourceTable.makeMinimalSchema()
+detectSourcesConfig = measAlg.SourceDetectionConfig(thresholdType='value')
+measureSourcesConfig = measAlg.SourceMeasurementConfig()
 
 # Setup the detection and measurement tasks
-detect  = measAlg.SourceDetectionTask(config=detectSourcesConfig,  
-                                      schema=schema)
-measure = measAlg.SourceMeasurementTask(config=measureSourcesConfig, 
+detect = measAlg.SourceDetectionTask(config=detectSourcesConfig,
+                                     schema=schema)
+measure = measAlg.SourceMeasurementTask(config=measureSourcesConfig,
                                         schema=schema)
 
 # Set flux aliases to None
-measureSourcesConfig.slots.psfFlux    = None
-measureSourcesConfig.slots.apFlux     = None
-measureSourcesConfig.slots.modelFlux  = None
-measureSourcesConfig.slots.instFlux   = None
+measureSourcesConfig.slots.psfFlux = None
+measureSourcesConfig.slots.apFlux = None
+measureSourcesConfig.slots.modelFlux = None
+measureSourcesConfig.slots.instFlux = None
 measureSourcesConfig.validate()
 
-#  Choose algorithms to look at the output of. 
-fields = [#'centroid.naive', 
-    #'centroid.naive.err', 'centroid.naive.flags',
-    #'centroid.gaussian', 
-    #'centroid.gaussian.err',
-    'centroid.sdss', 
+#  Choose algorithms to look at the output of.
+fields = [  # 'centroid.naive',
+    # 'centroid.naive.err', 'centroid.naive.flags',
+    # 'centroid.gaussian',
+    # 'centroid.gaussian.err',
+    'centroid.sdss',
     'centroid.sdss.flags',
     'shape.sdss',
-    'shape.sdss.err', 
-    'shape.sdss.centroid', 
+    'shape.sdss.err',
+    'shape.sdss.centroid',
     # 'shape.sdss.centroid.err',
     'shape.sdss.flags',
     'flux.gaussian']
-    
-algoKeys   = [schema.find(f).key for f in fields]
+
+algoKeys = [schema.find(f).key for f in fields]
 
 # Loop over the set of files in electron intensity and BF effect strength
 for (j, i) in itertools.product(extraId, electronLevel):
@@ -81,91 +81,92 @@ for (j, i) in itertools.product(extraId, electronLevel):
         print "using", j, i
 
     fileName = "%s%02d%d%s" % (outDir, i, j, suffix)
-    exposure    = afwImg.ExposureF(fileName)
+    exposure = afwImg.ExposureF(fileName)
     maskedImage = exposure.getMaskedImage()
 
     # These three are held in the maskedImage
-    image       = maskedImage.getImage()
-    mask        = maskedImage.getMask()
-    variance    = maskedImage.getVariance()
-    
-    # We need to manually make the variance plane since it is not in the phosim image.
-    variance.getArray()[:,:] = np.abs(image.getArray())
-        
+    image = maskedImage.getImage()
+    mask = maskedImage.getMask()
+    variance = maskedImage.getVariance()
+
+    # We need to manually make the variance plane since it is not in the
+    # phosim image.
+    variance.getArray()[:, :] = np.abs(image.getArray())
+
     imageStatistics = afwMath.makeStatistics(maskedImage, statFlags)
-    numBins         = imageStatistics.getResult(afwMath.NPOINT)[0]
-    mean            = imageStatistics.getResult(afwMath.MEAN)[0]
-    
+    numBins = imageStatistics.getResult(afwMath.NPOINT)[0]
+    mean = imageStatistics.getResult(afwMath.MEAN)[0]
+
     if printLevel >= 2:
         print "The image has dimensions %i x %i pixels" \
-            %(maskedImage.getWidth(), maskedImage.getHeight())
-        print "Number of analyzed bins in image is %i"  %numBins
-        print "Max    = %9d"            %imageStatistics.getResult(afwMath.MAX)[0]
-        print "Min    = %9d"            %imageStatistics.getResult(afwMath.MIN)[0]
-        print "Mean   = %9.8f +- %3.1f" %imageStatistics.getResult(afwMath.MEAN)
-        print "StdDev = %9.2f"          %imageStatistics.getResult(afwMath.STDEV)[0]
-        
-    # Detect the sources,then put them into a catalog 
+            % (maskedImage.getWidth(), maskedImage.getHeight())
+        print "Number of analyzed bins in image is %i" % numBins
+        print "Max  = %9d" % imageStatistics.getResult(afwMath.MAX)[0]
+        print "Min  = %9d" % imageStatistics.getResult(afwMath.MIN)[0]
+        print "Mean = %9.8f +- %3.1f" % imageStatistics.getResult(afwMath.MEAN)
+        print "StdDev = %9.2f" % imageStatistics.getResult(afwMath.STDEV)[0]
+
+    # Detect the sources,then put them into a catalog
     # (the table is where the catalog atually stores stuff)
-    table   = afwTable.SourceTable.make(schema)
+    table = afwTable.SourceTable.make(schema)
     catalog = detect.makeSourceCatalog(table, exposure, sigma=5)
-    
+
     # Get the sources out of the catalog
     sources = catalog.sources
-        
+
     # Apply the measurement routines to the exposure using the sources as input
     measure.run(exposure, sources)
-    
+
     # For now there should only be one source (otherwise this won't work right)
     for source in sources:
         if printLevel >= 1:
             print "Source found at ", source.getCentroid()
-    
-    x,y = source.getCentroid()
-    
+
+    x, y = source.getCentroid()
+
     # Now loop through the keys we want
-    for f,k in zip(fields, algoKeys):
+    for f, k in zip(fields, algoKeys):
         # print '    ', f, source.get(k)
-        if f=='shape.sdss':
+        if f == 'shape.sdss':
             ixx = math.sqrt(source.get(k).getIxx())
             iyy = math.sqrt(source.get(k).getIyy())
             ixy = source.get(k).getIxy()
 
-        if f=='shape.sdss.err':            
-            errxx = math.sqrt(source.get(k)[0,0]) 
-            erryy = math.sqrt(source.get(k)[1,1])
-            errxy = math.sqrt(source.get(k)[2,2])
-        
+        if f == 'shape.sdss.err':
+            errxx = math.sqrt(source.get(k)[0, 0])
+            erryy = math.sqrt(source.get(k)[1, 1])
+            errxy = math.sqrt(source.get(k)[2, 2])
+
             if (math.isnan(errxx) or math.isnan(erryy)):
                 print "Caught a NAN!"
                 errxx = 0
                 erryy = 0
-                    
+
     # Calculate the sizes myself by oversampling.  This is
     # necessary for single pixel size spots where the default algorithms fail.
 
     # Had to add this for dev branch
-    x = round (x)
-    y = round (y)
+    x = round(x)
+    y = round(y)
 
-    yaxis      = np.linspace(y-10,y+10,200)
-    yvalues    = np.repeat(image.getArray()[y-10:y+10,x],10)
-    myaverage  = np.average(yaxis, weights=yvalues)
-    variancey  = np.average((yaxis-myaverage)**2, weights=yvalues)
-    stdy       = math.sqrt(variancey)
-    
-    xaxis      = np.linspace(x-10,x+10,200)
-    xvalues    = np.repeat(image.getArray()[y,x-10:x+10],10)
-    myaverage =  np.average(xaxis, weights=xvalues)
-    variancex  = np.average((xaxis-myaverage)**2, weights=xvalues)
-    stdx       = math.sqrt(variancex)
-            
-    print ("ID:%1d Electrons= %6s STDX= %4.2f STDY= %4.2f IXX= %4.2f IYY= %4.2f "
-            "IXY= %4.2f") % (j, i, stdx, stdy, ixx, iyy, ixy)
+    yaxis = np.linspace(y - 10, y + 10, 200)
+    yvalues = np.repeat(image.getArray()[y - 10:y + 10, x], 10)
+    myaverage = np.average(yaxis, weights=yvalues)
+    variancey = np.average((yaxis - myaverage)**2, weights=yvalues)
+    stdy = math.sqrt(variancey)
+
+    xaxis = np.linspace(x - 10, x + 10, 200)
+    xvalues = np.repeat(image.getArray()[y, x - 10:x + 10], 10)
+    myaverage = np.average(xaxis, weights=xvalues)
+    variancex = np.average((xaxis - myaverage)**2, weights=xvalues)
+    stdx = math.sqrt(variancex)
+
+    print ("ID:%1d Electrons= %6s STDX= %4.2f STDY= %4.2f IXX= %4.2f "
+           "IYY= %4.2f IXY= %4.2f") % (j, i, stdx, stdy, ixx, iyy, ixy)
 
     # Fill the Pandas data frame
-    spots.loc[ len(spots) ] = (j, i, ixx, errxx, iyy, erryy, stdx, stdy)
-    
+    spots.loc[len(spots)] = (j, i, ixx, errxx, iyy, erryy, stdx, stdy)
+
 # Write out the data store
 h5store['spots'] = spots
 h5store.close()
--- subtractFlats.py
+++ subtractFlats.py
@@ -3,82 +3,88 @@
 # You should 'setup pipe_test' to use it.
 # C. Walter 01/2014
 
-import math           as math
-import numpy          as np
-import pandas         as pd
+import numpy as np
+import pandas as pd
 import itertools
 
-import lsst.afw.math  as afwMath
+import lsst.afw.math as afwMath
 import lsst.afw.image as afwImg
 
-def submatrix(M,i,j):
-    return M[i-1:i+2,j-1:j+2].ravel()
+
+def submatrix(M, i, j):
+    return M[i - 1:i + 2, j - 1:j + 2].ravel()
+
 
 def processImage(maskedImage):
+
+    global statFlags
+    global printLevel
 
     # We want to remove the edges because of roll-off effects
     # Trim 20 pixels from outside
     trimmedImage = maskedImage[20:-20, 20:-20]
-    
-    # These three are held in the maskedImage
-    image       = trimmedImage.getImage()
-    mask        = trimmedImage.getMask()
-    variance    = trimmedImage.getVariance()
-    
-    imageStatistics = afwMath.makeStatistics(trimmedImage, statFlags)
-    numBins         = imageStatistics.getResult(afwMath.NPOINT)[0]
-    mean            = imageStatistics.getResult(afwMath.MEAN)[0]
 
-    if printLevel >= 2:    
-        print "The image has dimensions %i x %i pixels" \
-            %(trimmedImage.getWidth(), trimmedImage.getHeight())
-        print "Number of analyzed bins in image is %i"  %numBins
-        print "Max    = %9d"            %imageStatistics.getResult(afwMath.MAX)[0]
-        print "Min    = %9d"            %imageStatistics.getResult(afwMath.MIN)[0]
-        print "Mean   = %9.3f +- %3.3f" %imageStatistics.getResult(afwMath.MEAN)
-        print "StdDev = %9.2f"          %imageStatistics.getResult(afwMath.STDEV)[0]
+    # These three are held in the maskedImage
+    image = trimmedImage.getImage()
+    mask = trimmedImage.getMask()
+    variance = trimmedImage.getVariance()
+
+    imageStatistics = afwMath.makeStatistics(trimmedImage, statFlags)
+    numBins = imageStatistics.getResult(afwMath.NPOINT)[0]
+
+    if printLevel >= 2:
+        print "The image has dimensions %i x %i pixels" % \
+            (trimmedImage.getWidth(), trimmedImage.getHeight())
+        print "Number of analyzed bins in image is %i" % numBins
+        print "Max  = %9d" % imageStatistics.getResult(afwMath.MAX)[0]
+        print "Min  = %9d" % imageStatistics.getResult(afwMath.MIN)[0]
+        print "Mean = %9.3f +- %3.3f" % imageStatistics.getResult(afwMath.MEAN)
+        print "StdDev = %9.2f" % imageStatistics.getResult(afwMath.STDEV)[0]
 
     a = image.getArray().T
-    
-    if printLevel >= 2:        
+
+    if printLevel >= 2:
         print "\nCalculate 2D spatial Autocorrelation"
         print "shape is", a.shape
 
     np.set_printoptions(precision=3, suppress=True)
 
-    # Take a 3x3 matrix around each pixel and then contruct a matrix for which
-    # the row is one of those pixels labels  and the column is the value for this
-    # pixel.  Repeat this for each pixel.  The result is a 3x3=9 rows and 
-    # N columns (one for each pixel). Ignore the outer edges since they don't
-    # have the the full set of neighbors.
+    # Take a 3x3 matrix around each pixel and then contruct a matrix
+    # for which the row is one of those pixels labels and the column
+    # is the value for this pixel.  Repeat this for each pixel.  The
+    # result is a 3x3=9 rows and  N columns (one for each
+    # pixel). Ignore the outer edges since they don't have the the
+    # full set of neighbors.
 
-    q = [ submatrix(a,i,j) for i,j in np.ndindex(a.shape) 
-        if i != 0 and j != 0 and i != a.shape[0]-1 and j != a.shape[1]-1]
+    q = [submatrix(a, i, j) for i, j in np.ndindex(a.shape)
+         if i != 0 and j != 0 and i != a.shape[0] - 1 and j != a.shape[1] - 1]
 
     y = np.column_stack(q)
 
-    fullCorrelation        = np.corrcoef(y)
-    centerPixelCorrelation = fullCorrelation[5-1].reshape(3,3)
+    fullCorrelation = np.corrcoef(y)
+    centerPixelCorrelation = fullCorrelation[5 - 1].reshape(3, 3)
 
-    if printLevel >= 2:    
+    if printLevel >= 2:
         print centerPixelCorrelation
 
     # Now make a new 100x104 matrix to check the mean and stddev.
     # (group each pixel into 4x4 blocks)
     # I barely understand how this works (CWW)!
     rows, cols = a.shape
-    b = a.reshape(rows//4,4,cols//4,4).sum(axis=(1, 3))
-        
-    if printLevel >= 2:    
+    b = a.reshape(rows // 4, 4, cols // 4, 4).sum(axis=(1, 3))
+
+    if printLevel >= 2:
         print "Original Mean:", np.mean(a), "Std:", np.std(a)
         print "4x4      Mean:", np.mean(b), "Std:", np.std(b)
-        print 
+        print
 
-    horizCorrelation = (centerPixelCorrelation[1,0] + centerPixelCorrelation[1,2])/2.0
-    vertCorrelation  = (centerPixelCorrelation[0,1] + centerPixelCorrelation[2,1])/2.0
+    horizCorrelation = (centerPixelCorrelation[1, 0] + centerPixelCorrelation[1, 2]) / 2.0
+
+    vertCorrelation = (centerPixelCorrelation[0, 1] + centerPixelCorrelation[2, 1]) / 2.0
 
     return (np.mean(a), np.std(a), np.mean(b), np.std(b),
             horizCorrelation, vertCorrelation)
+
 
 # Main Program
 def main():
@@ -86,25 +92,24 @@ def main():
     global statFlags
     global printLevel
 
-    printLevel = 0
-
     # Make a Panda hd5f data store and frame.
     h5store = pd.HDFStore('flatData.h5', mode='w')
-    flats   = pd.DataFrame(columns =
-            ('config', 'magnitude', 'numElectrons', 'PTC', 'groupPTC', 'hCorr', 'vCorr'))
- 
-    # Setup global statistics and filenames    
-    statFlags = (afwMath.NPOINT | afwMath.MEAN | afwMath.STDEV | afwMath.MAX | 
-    afwMath.MIN | afwMath.ERRORS)
-    print "The statistics flags are set to %s."%bin(statFlags)
+    flats = pd.DataFrame(columns=('config', 'magnitude',
+                                  'numElectrons', 'PTC', 'groupPTC',
+                                  'hCorr', 'vCorr'))
+
+    # Setup global statistics and filenames
+    statFlags = (afwMath.NPOINT | afwMath.MEAN | afwMath.STDEV | afwMath.MAX |
+                 afwMath.MIN | afwMath.ERRORS)
+    print "The statistics flags are set to %s." % bin(statFlags)
     print "Errors will be calculated.\n"
 
-    outDir       = 'output/lsst_flats_e_'
-    suffix       = '_f2_R22_S11_E000.fits.gz'
+    outDir = 'output/lsst_flats_e_'
+    suffix = '_f2_R22_S11_E000.fits.gz'
 
     # Process Files
     magnitude = [18, 15, 14, 13, 12, 10]
-    extraId   = [0, 1, 2, 3, 4]
+    extraId = [0, 1, 2, 3, 4]
 
     # Loop over the set of files in electron intensity and BF effect strength
     for (j, i) in itertools.product(extraId, magnitude):
@@ -119,43 +124,52 @@ def main():
         maskedImage3 -= maskedImage2
 
         # Process images
-        if printLevel >= 1: print "Processing file ", fileName1
-        (mean1,std1,groupMean1,groupStd1,hCorr1,vCorr1) = processImage(maskedImage1)
-        if printLevel >= 1: print "Processing file ", fileName2
-        (mean2,std2,groupMean2,groupStd2,hCorr2,vCorr2) = processImage(maskedImage2)
-        if printLevel >= 1: print "Processing Difference"
-        (mean3, std3, groupMean3,groupStd3,hCorr3,vCorr3) = processImage(maskedImage3)
+        if printLevel >= 1:
+            print "Processing file ", fileName1
+        (mean1, std1, groupMean1, groupStd1, hCorr1, vCorr1) = processImage(maskedImage1)
 
-        #Calculate PTC entry (Mean/Variance)
-        PTC1      = mean1/std1**2
-        PTC3      = (mean1+mean2)/std3**2
-        groupPTC1 = groupMean1/groupStd1**2
-        groupPTC3 = (groupMean1+groupMean2)/groupStd3**2
+        if printLevel >= 1:
+            print "Processing file ", fileName2
+        (mean2, std2, groupMean2, groupStd2, hCorr2, vCorr2) = processImage(maskedImage2)
+
+        if printLevel >= 1:
+            print "Processing Difference"
+        (mean3, std3, groupMean3, groupStd3, hCorr3, vCorr3) = processImage(maskedImage3)
+
+        # Calculate PTC entry (Mean/Variance)
+        PTC1 = mean1 / std1**2
+        PTC3 = (mean1 + mean2) / std3**2
+        groupPTC1 = groupMean1 / groupStd1**2
+        groupPTC3 = (groupMean1 + groupMean2) / groupStd3**2
 
         # Print results
         if printLevel >= 1:
-            print "\n---Results for magnitude", i, "config", j,":\n"
+            print "\n---Results for magnitude", i, "config", j, ":\n"
 
-            print "Image1:\t %9.2f %9.2f %7.2f   "% (mean1, std1, PTC1)
-            print "Image3:\t %9.2f %9.2f %7.2f \n"% (mean3, std3, PTC3)
+            print "Image1:\t %9.2f %9.2f %7.2f   " % (mean1, std1, PTC1)
+            print "Image3:\t %9.2f %9.2f %7.2f \n" % (mean3, std3, PTC3)
 
-            print "Group1:\t %9.2f %9.2f %7.2f   "% (groupMean1, groupStd1, groupPTC1)
-            print "Group3:\t %9.2f %9.2f %7.2f \n"% (groupMean3, groupStd3, groupPTC3)
+            print "Group1:\t %9.2f %9.2f %7.2f   " % (groupMean1, groupStd1, groupPTC1)
+            print "Group3:\t %9.2f %9.2f %7.2f \n" % (groupMean3, groupStd3, groupPTC3)
 
-            print "Correlation1:\t %9.3f %9.3f"% (hCorr1, vCorr1)
-            print "Correlation3:\t %9.3f %9.3f"% (hCorr3, vCorr3)
+            print "Correlation1:\t %9.3f %9.3f" % (hCorr1, vCorr1)
+            print "Correlation3:\t %9.3f %9.3f" % (hCorr3, vCorr3)
             print
 
         # Print Summary Line for this set of files
         print "%d %d %8.2f %7.2f %7.2f %9.3f %9.3f %7.2f %9.3f %9.3f" % \
-        (i, j, mean1, std1, PTC1, hCorr1, vCorr1, PTC3, hCorr3, vCorr3)
+            (i, j, mean1, std1, PTC1, hCorr1, vCorr1, PTC3, hCorr3, vCorr3)
 
         # Fill the Pandas data frame
-        flats.loc[ len(flats) ] = (j, i, mean1, PTC3, groupPTC3, hCorr3, vCorr3)
+        flats.loc[len(flats)] = (j, i, mean1, PTC3, groupPTC3, hCorr3, vCorr3)
 
     # Write out the data store
     h5store['flats'] = flats
     h5store.close()
 
 if __name__ == "__main__":
+
+    printLevel = 0
+    statFlags = 0
+
     main()

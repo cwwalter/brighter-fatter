a93a0987e1b4e5224889dbd3de095fc8ecaeae1c
Chris Walter <chris.walter@duke.edu>
2015-06-05 15:08:11 +0900

    Update the spot processing code to version 10.1 of the stack.  The measurement algorithms have changed and this code uses the new framework.

--- processSpots.py
+++ processSpots.py
@@ -1,7 +1,7 @@
-# DM processing of Phosim output
+# DM processing of Phosim spot output
 # You should 'setup pipe_test' to use it.
-# This program
-# C. Walter 01/2014
+#
+# C. Walter: last major update to use DM v10.1 - 06/2015
 
 import math
 import numpy as np
@@ -13,6 +13,7 @@ import lsst.afw.table as afwTable
 import lsst.afw.image as afwImg
 
 import lsst.meas.algorithms as measAlg
+import lsst.meas.base as measBase
 import lsst.pex.logging as pexLog
 
 # Set the program verbosity level
@@ -43,36 +44,42 @@ print "Errors will be calculated.\n"
 # Configure the detection and measurement algorithms
 schema = afwTable.SourceTable.makeMinimalSchema()
 detectSourcesConfig = measAlg.SourceDetectionConfig(thresholdType='value')
-measureSourcesConfig = measAlg.SourceMeasurementConfig()
+measureSourcesConfig = measBase.SingleFrameMeasurementConfig()
 
-# Setup the detection and measurement tasks
-detect = measAlg.SourceDetectionTask(config=detectSourcesConfig,
-                                     schema=schema)
-measure = measAlg.SourceMeasurementTask(config=measureSourcesConfig,
-                                        schema=schema)
+# Choose algorithms to use
+measureSourcesConfig.plugins.names = ["base_GaussianCentroid", "base_SdssShape"]
+measureSourcesConfig.slots.centroid = "base_GaussianCentroid"
 
-# Set flux aliases to None
+# Set flux aliases to None. This is necessary since by defualt these point at
+# some alogorithims that are not in my list above.
 measureSourcesConfig.slots.psfFlux = None
 measureSourcesConfig.slots.apFlux = None
 measureSourcesConfig.slots.modelFlux = None
 measureSourcesConfig.slots.instFlux = None
 measureSourcesConfig.validate()
 
+# Setup the detection and measurement tasks
+detect = measAlg.SourceDetectionTask(config=detectSourcesConfig,
+                                     schema=schema)
+measure = measBase.SingleFrameMeasurementTask(config=measureSourcesConfig,
+                                              schema=schema)
 #  Choose algorithms to look at the output of.
-fields = [  # 'centroid.naive',
-    # 'centroid.naive.err', 'centroid.naive.flags',
-    # 'centroid.gaussian',
-    # 'centroid.gaussian.err',
-    'centroid.sdss',
-    'centroid.sdss.flags',
-    'shape.sdss',
-    'shape.sdss.err',
-    'shape.sdss.centroid',
-    # 'shape.sdss.centroid.err',
-    'shape.sdss.flags',
-    'flux.gaussian']
+fields = ['base_GaussianCentroid', 'base_SdssShape']
 
-algoKeys = [schema.find(f).key for f in fields]
+# Find the keys for those algorithms.
+algoKeys = []
+for f in fields:
+    if f == 'base_GaussianCentroid':
+        k = afwTable.Point2DKey(schema[f])
+    elif f == 'base_SdssShape':
+        k = measBase.SdssShapeResultKey(schema[f])
+    else:
+        k = schema.find(f).key
+    algoKeys.append(k)
+
+if printLevel >= 2:
+    for i, key in enumerate(zip(fields, algoKeys)):
+        print i, key
 
 # Loop over the set of files in electron intensity and BF effect strength
 for (j, i) in itertools.product(extraId, electronLevel):
@@ -80,8 +87,18 @@ for (j, i) in itertools.product(extraId, electronLevel):
     if printLevel >= 1:
         print "using", j, i
 
+    # Make a afw Exposure
     fileName = "%s%02d%d%s" % (outDir, i, j, suffix)
     exposure = afwImg.ExposureF(fileName)
+
+    # Add a Gaussian PSF to the exposure.  This is needed by the SDSS Centroid Algorithm.
+    # For the kernel make it 3*sigma on either side of the central pixel.
+    sigma = 1.0
+    size = 2*int(3*sigma) + 1
+    gaussianPSF = measAlg.SingleGaussianPsf(size, size, sigma)
+    exposure.setPsf(gaussianPSF)
+
+    # Make a maskedImage
     maskedImage = exposure.getMaskedImage()
 
     # These three are held in the maskedImage
@@ -127,27 +144,34 @@ for (j, i) in itertools.product(extraId, electronLevel):
     # Now loop through the keys we want
     for f, k in zip(fields, algoKeys):
         # print '    ', f, source.get(k)
-        if f == 'shape.sdss':
-            ixx = math.sqrt(source.get(k).getIxx())
-            iyy = math.sqrt(source.get(k).getIyy())
-            ixy = source.get(k).getIxy()
+        if f == 'base_SdssShape':
+            result = source.get(k)
 
-        if f == 'shape.sdss.err':
-            errxx = math.sqrt(source.get(k)[0, 0])
-            erryy = math.sqrt(source.get(k)[1, 1])
-            errxy = math.sqrt(source.get(k)[2, 2])
+            # Currently bug in DM sets this flag every time
+            # errorFlag = result.getFlag(measBase.SdssShapeAlgorithm.FAILURE)
+            # if errorFlag is True:
+            #     print "Error on File!"
 
-            if (math.isnan(errxx) or math.isnan(erryy)):
-                print "Caught a NAN!"
-                errxx = 0
-                erryy = 0
+            if printLevel >= 2:
+                print dir(result)
+                print result.x, result.y
+                print result.getCentroid()
+                print result.xx, result.yy, result.xy
+                print result.xxSigma, result.yySigma
+                print result.getShape()
+                print result.getShapeErr()
+                print result.getQuadrupole()
+
+            ixx = math.sqrt(result.xx)
+            iyy = math.sqrt(result.yy)
+            ixy = result.xy  # This could be be negative
+
+            errxx = result.xxSigma
+            erryy = result.yySigma
+            errxy = result.xySigma
 
     # Calculate the sizes myself by oversampling.  This is
     # necessary for single pixel size spots where the default algorithms fail.
-
-    # Had to add this for dev branch
-    x = round(x)
-    y = round(y)
 
     yaxis = np.linspace(y - 10, y + 10, 200)
     yvalues = np.repeat(image.getArray()[y - 10:y + 10, x], 10)

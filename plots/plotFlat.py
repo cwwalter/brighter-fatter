fc8eb10ac59352526c6f6b54014324f521deb1ab
Chris Walter <chris.walter@duke.edu>
2015-08-11 15:06:56 -0400

    Modify flats plotting for X1 only.

--- plots/plotFlat.py
+++ plots/plotFlat.py
@@ -1,51 +1,55 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
 def plotFrameVariable(dataFrame, title, variable, yLabel, yLim):
 
     config0 = dataFrame.query('config==0')
     config1 = dataFrame.query('config==1')
-    config2 = dataFrame.query('config==2')
-    config3 = dataFrame.query('config==3')
-    config4 = dataFrame.query('config==4')
+    # config2 = dataFrame.query('config==2')
+    # config3 = dataFrame.query('config==3')
+    # config4 = dataFrame.query('config==4')
 
-    flatPlot, axis = plt.subplots(1,1)
+    flatPlot, axis = plt.subplots(1, 1)
     flatPlot.suptitle(title)
 
     config0.plot('numElectrons', variable, style='ro', ax=axis, label='Perfect')
     config1.plot('numElectrons', variable, style='go', ax=axis, label='x1')
-    config2.plot('numElectrons', variable, style='bo', ax=axis, label='x10')
-    #config3.plot('numElectrons', variable, style='co', ax=axis, label='x100')
-    #config3.plot('numElectrons', variable, style='yo', ax=axis, label='x500')
+    # config2.plot('numElectrons', variable, style='bo', ax=axis, label='x10')
+    # config3.plot('numElectrons', variable, style='co', ax=axis, label='x100')
+    # config3.plot('numElectrons', variable, style='yo', ax=axis, label='x500')
 
     axis.set_xlabel('Number of Electrons')
     axis.set_ylabel(yLabel)
-    axis.set_xlim(0.0, 230000)
+    axis.set_xlim(0.0, 250000)
     axis.set_ylim(yLim)
     axis.grid('off', axis='both')
-    axis.legend(loc='best', prop={'size':12}, numpoints=1)
+    axis.legend(loc='best', prop={'size': 12}, numpoints=1)
+
+    plt.plot([0.0, 250000.0], [0.0, 0.0], color='grey', linestyle='--')
 
     flatPlot.show()
+
 
 # Main Program
 def main():
 
     h5store = pd.HDFStore('flatData.h5')
-    flats   = h5store['flats']
-    
+    flats = h5store['flats']
+
     plotFrameVariable(flats, 'PTC Curve for simulated flats',
-                'PTC', 'Mean / Variance', (0.0, 2.8) )
-    
+                      'PTC', 'Mean / Variance', (0.0, 2.8))
+
     plotFrameVariable(flats, 'PTC Curve for simulated flats grouped into 4x4 pixels',
-                'groupPTC', 'Mean / Variance', (0.0, 2.8) )
-    
+                      'groupPTC', 'Mean / Variance', (0.0, 2.8))
+
     plotFrameVariable(flats, 'Horizontal Autocorrelation Coefficients',
-                'hCorr', 'Autocorrelation Coefficient', (-0.1, 0.15) )
-    
+                      'hCorr', 'Autocorrelation Coefficient', (-0.01, 0.06))
+
     plotFrameVariable(flats, 'Vertical Autocorrelation Coefficients',
-                'vCorr', 'Autocorrelation Coefficient', (-0.1, 0.15) )
+                      'vCorr', 'Autocorrelation Coefficient', (-0.01, 0.06))
 
     h5store.close()
-    
+
 if __name__ == "__main__":
     main()
